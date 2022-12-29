6323262aed0f15cff68b7a87695ef369b71701b7
djhedges <djhedges@gmail.com>
2022-12-28 21:00:34 -0800

    Add fuse, pdm & kill switch

--- corrado_wiring.py
+++ corrado_wiring.py
@@ -9,7 +9,7 @@ class Node(object):
     label = f'{name} '
     for pin_name in pin_names:
       label += f'| <{pin_name}> {pin_name} '
-    G.add_node(name, label=label)
+    G.add_node(name, label=label, ranksep=2.0)
 
 
 def AddPath(node_pins, color):
@@ -21,14 +21,56 @@ def AddPath(node_pins, color):
 
 G.node_attr['shape'] = 'record'
 Node('battery', ['pos', 'neg'])
-Node('connector', ['1', '2'])
-Node('fuel_pump', ['pos', 'neg'])
+Node('main_fuse', ['fuse'])
+Node('alternator', ['pos', 'sense'])
+# TODO: Verify the pins match this diagram.
+# https://www.pegasusautoracing.com/document.asp?DocID=TECH00109
+Node('kill_switch', ['battery', 'z', 'w', 'starter'])
+# TODO: Wire ignition coils pdm > z > coils.
+Node('kill_switch_resistor', ['resistor'])
+Node('ign_switch', ['1', '2'])
+Node('razor_pdm', [
+    'pos', 'neg',
+    'PWROUT2a', 'NC1', 'CANH', 'IGNSW', 'SENSOR 5V1', 'SENSOR GND1', 'PWROUT3a,'
+    'PWROUT2b', 'NC2', 'CANL', 'SENSOR 5V2', 'SENSOR GND2', 'PWROUT3b,'
+    'PWROUT1a', 'ADIO2', 'ADIO4', 'ADIO6', 'ADIO8', 'PWROUT4a,'
+    'PWROUT1b', 'ADIO1', 'ADIO3', 'ADIO5', 'ADIO7', 'NC3', 'PWROUT4b'
+])
+# TODO: Connect fuel pump.
+# Node('fuel_pump', ['pos', 'neg'])
 
 AddPath((
   ('battery', 'pos'),
-  ('connector', '1'),
-  ('fuel_pump', 'pos'),
+  ('main_fuse', 'fuse'),
+  ('kill_switch', 'battery'),
 ), 'red')
+AddPath((
+  ('alternator', 'pos'),
+  ('kill_switch', 'starter'),
+), 'red')
+AddPath((
+  ('kill_switch', 'starter'),
+  ('kill_switch', 'w'),
+  ('kill_switch_resistor', 'resistor'),
+), 'red')
+AddPath((
+  ('kill_switch', 'starter'),
+  ('razor_pdm', 'pos'),
+), 'red')
+AddPath((
+  ('battery', 'neg'),
+  ('razor_pdm', 'neg'),
+), 'black')
+
+# TODO: Verify blue is not in use for IGNSW.
+AddPath((
+  ('kill_switch', 'z'),
+  ('ign_switch', '1'),
+), 'blue')
+AddPath((
+  ('ign_switch', '2'),
+  ('razor_pdm', 'IGNSW'),
+), 'blue')
 
 G.layout(prog='dot')
 G.write('corrado_wiring.dot')

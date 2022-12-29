7ebaa958f96ec634e473a8b58bdb5f03ac553d31
djhedges <djhedges@gmail.com>
2022-12-28 20:02:21 -0800

    Early attempt to codify the graphviz generation which helps reduce what we need to specify

--- corrado_wiring.py
+++ corrado_wiring.py
@@ -4,13 +4,31 @@ import pygraphviz as pgv
 
 G = pgv.AGraph()
 
-G = pgv.AGraph()
+class Node(object):
+  def __init__(self, name, pin_names):
+    label = f'{name} '
+    for pin_name in pin_names:
+      label += f'| <{pin_name}> {pin_name} '
+    G.add_node(name, label=label)
+
+
+def AddPath(node_pins, color):
+  for i in range(len(node_pins) - 1):
+      node, pin = node_pins[i]
+      next_node, next_pin = node_pins[i + 1]
+      G.add_edge(node, next_node, tailport=pin, headport=next_pin, color=color, penwidth=5)
+
 
 G.node_attr['shape'] = 'record'
-G.add_node('battery', label='{battery | {<pos> + |<neg> - }}')
-G.add_node('fuel_pump', label='{fuel pump | {<pos> + |<neg> - }}')
+Node('battery', ['pos', 'neg'])
+Node('connector', ['1', '2'])
+Node('fuel_pump', ['pos', 'neg'])
 
-G.add_edge('battery', 'fuel_pump', headport='pos', tailport='pos', color='red', penwidth=5)
+AddPath((
+  ('battery', 'pos'),
+  ('connector', '1'),
+  ('fuel_pump', 'pos'),
+), 'red')
 
 G.layout(prog='dot')
 G.write('corrado_wiring.dot')
